2a8fb40d4e6100053f5902b611d5155cb79eea41
Saraesp <saraesposito145@gmail.com>
2023-01-27 18:58:29 +0100

    aggiunta impostazione file censura parola e lunghezza paragrafo

--- censura.php
+++ censura.php
@@ -1,2 +1,24 @@
+<?php
+    $paragrafo = $_POST ['paragrafo'];
+    $censura = $_POST ['censura'];
 
+    $new_paragrafo = str_replace($censura, '***', $paragrafo);
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>PARAGRAFO SENZA CENSURA E LUNGHEZZA</h3>
+    <p><?php echo $paragrafo?></p>
+    <p><?php echo strlen($paragrafo)?></p>
+    <h3>PARAGRAFO CON CENSURA E LUNGHEZZA</h3>
+    <p><?php echo $new_paragrafo?></p>
+    <p><?php echo strlen($new_paragrafo)?></p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <form action="" class="p-4">
+    <form action="./censura.php" method="POST" class="p-4">
         <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label">Scrivi Paragrafo</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" id="paragrafo" name="paragrafo"></textarea>
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Parola da Censurare</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
+            <input type="text" class="form-control" id="exampleFormControlInput1" id="censura" name="censura">
         </div>
         <button class="btn btn-primary" type="submit">Invia</button>
 
